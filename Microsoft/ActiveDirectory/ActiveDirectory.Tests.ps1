8274fb376c85405e1dbd57c2a09b36695f9a2089
Tore Groneng <tore.groneng@gmail.com>
2016-04-27 10:44:19 +0200

    Updated tests for FIM. Added Database tier tests

--- Microsoft/ActiveDirectory/ActiveDirectory.Tests.ps1
+++ Microsoft/ActiveDirectory/ActiveDirectory.Tests.ps1
@@ -197,4 +197,94 @@ Describe "Active Directory Operational Validation" {
             }
         } 
     }
+    
+    $SqlServerName = "sqlfimserver.domain.no"
+    
+    Context "Database tier $SqlServerName" {
+        
+
+        It "$SqlServerName should be online" {
+            Test-Connection -ComputerName $SqlServerName -Quiet -Count 1 | Should be $true
+        }
+
+        $serviceName = "MSSQLSERVER"
+        $serviceAccount = "domain\sqlservice"
+        $CimService = Get-CimInstance -ClassName win32_Service -Filter "Name like '$ServiceName'" -ComputerName $SqlServerName
+
+        It "Service $ServiceName should be running" {
+            $CimService.State | Should Be "Running"
+        }
+
+        It "Service $ServiceName should start automatically" {
+            $CimService.StartMode | Should Be "Auto"
+        }
+
+        It "Service $ServiceName should be running as $serviceAccount" {
+            $CimService.StartName | Should Be $serviceAccount
+        }
+
+        $serviceName = "SQLSERVERAGENT"
+        $serviceAccount = "domain\sqlservice"
+        $CimService = Get-CimInstance -ClassName win32_Service -Filter "Name like '$ServiceName'" -ComputerName $SqlServerName
+
+        It "Service $ServiceName should be running" {
+            $CimService.State | Should Be "Running"
+        }
+
+        It "Service $ServiceName should start automatically" {
+            $CimService.StartMode | Should Be "Auto"
+        }
+
+        It "Service $ServiceName should be running as $serviceAccount" {
+            $CimService.StartName | Should Be $serviceAccount
+        }     
+
+        $serviceName = "MSSQLServerOLAPService"
+        $serviceAccount = "domain\sqlservice"
+        $CimService = Get-CimInstance -ClassName win32_Service -Filter "Name like '$ServiceName'" -ComputerName $SqlServerName
+
+        It "Service $ServiceName should be running" {
+            $CimService.State | Should Be "Running"
+        }
+
+        It "Service $ServiceName should start automatically" {
+            $CimService.StartMode | Should Be "Auto"
+        }
+
+        It "Service $ServiceName should be running as $serviceAccount" {
+            $CimService.StartName | Should Be $serviceAccount
+        }    
+        
+        $serviceName = "MsDtsServer110"
+        $serviceAccount = "domain\sqlservice"
+        $CimService = Get-CimInstance -ClassName win32_Service -Filter "Name like '$ServiceName'" -ComputerName $SqlServerName
+
+        It "Service $ServiceName should be running" {
+            $CimService.State | Should Be "Running"
+        }
+
+        It "Service $ServiceName should start automatically" {
+            $CimService.StartMode | Should Be "Auto"
+        }
+
+        It "Service $ServiceName should be running as $serviceAccount" {
+            $CimService.StartName | Should Be $serviceAccount
+        }    
+        
+        $serviceName = "ReportServer"
+        $serviceAccount = "domain\sqlservice"
+        $CimService = Get-CimInstance -ClassName win32_Service -Filter "Name like '$ServiceName'" -ComputerName $SqlServerName
+
+        It "Service $ServiceName should be running" {
+            $CimService.State | Should Be "Running"
+        }
+
+        It "Service $ServiceName should start automatically" {
+            $CimService.StartMode | Should Be "Auto"
+        }
+
+        It "Service $ServiceName should be running as $serviceAccount" {
+            $CimService.StartName | Should Be $serviceAccount
+        }
+    }
 }
